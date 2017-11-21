76e72a902c8f5e52228fecc6fcf6be1c971f27b7
Paweł Konik <konius85@gmail.com>
2017-11-21 18:12:18 +0100

    Add components folder and style.css

--- setup.sh
+++ setup.sh
@@ -4,12 +4,13 @@ mkdir $1
 cd $1
 touch index.js
 touch .gitignore
-
+touch style.css
+mkdir components
 echo ".gitignore" >> .gitignore
+echo "package.json" >> .gitignore
 npm init -y
+
 
 git init
 git add -A
 git commit -m "Initalize project $1"
-
-
